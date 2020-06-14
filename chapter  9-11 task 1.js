83316f54c3df3975dbdd78b96596afab967bd21f
NasirAliPanhwar <65031269+NasirAliPanhwar@users.noreply.github.com>
2020-06-14 16:24:05 +0500

    Add files via upload

---
+++ chapter  9-11 task 1.js
@@ -0,0 +1,18 @@
+prompt("Enter the city name:")
+
+
+var city ="Karachi";
+
+if ( city == "Karachi")
+
+
+{
+
+    document.write(" Welcome to city of lights");
+}
+
+else{
+ document.write("Welcome");
+
+    
+}
